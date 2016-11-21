abd365a0d43ffaa45aad8dbee160069b461c752f
Boina <nicolas.alarcon.14@sansano.usm.cl>
2016-11-21 09:00:47 -0300

    borrar y editar modulo alpha

--- SADA/app/ramos/modulo.js
+++ SADA/app/ramos/modulo.js
@@ -28,4 +28,55 @@ module.exports = function(app, passport, connection, transporter, dbconfig, titl
             unidad_nombre:req.body.unidad_nombre
         });
     });
+
+    app.post('/editarModulo', isLoggedIn, function(req, res) {
+
+        var posComa = req.body.modulitos.indexOf(",");
+        var id = req.body.modulitos.slice(0, posComa);
+        console.log(id);
+        connection.query('SELECT * FROM modulo WHERE idModulo =?;',id ,function (err, rows) {
+            if (err) throw err;
+
+
+            console.log('holaza');
+
+            res.render("ramos/editarModulo.ejs", {
+                title: title,
+                user: req.user,
+                moduloObjetivo:rows,
+                unidad_id:req.body.unidad_id,
+                unidad_nombre:req.body.unidad_nombre
+            });
+
+        });
+
+    });
+    app.post('/editModulo', isLoggedIn, function(req, res) {
+
+        var tipo = req.body.tipo;
+        var id = req.body.modulo_id;
+        var contenido = req.body.contenido;
+
+        connection.query('UPDATE modulo SET Tipo=?,Informacion=? WHERE idModulo =?;',[tipo, contenido, id] ,function (err, rows) {
+            if (err) throw err;
+
+            res.redirect('/');
+        });
+
+    });
+    app.post('/borrarModulo', isLoggedIn, function(req, res) {
+
+        var id = req.body.modulo_id;
+
+        connection.query('DELETE FROM ensamblaje WHERE Modulo_idModulo =?;',id ,function (err, rows) {
+            if (err) throw err;
+        });
+
+        connection.query('DELETE FROM modulo WHERE idModulo =?;',id ,function (err, rows) {
+            if (err) throw err;
+        });
+
+        res.redirect('/');
+
+    });
 }
