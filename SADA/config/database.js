ee5242df45634265b27708f21127023446bc1997
Rodrigo Elicer Krause <rodrigoelicer@users.noreply.github.com>
2016-11-25 12:43:05 -0300

    Add files via upload

---
+++ SADA/config/database.js
@@ -0,0 +1,11 @@
+
+// config/database.js
+module.exports = {
+    'connection': {
+        'host': 'localhost',
+        'user': 'user',
+        'password': 'password'
+    },
+    'database': 'Sada_DB',
+    'users_table': 'usuario'
+};
